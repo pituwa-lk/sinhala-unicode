849dfc7e86efc05aa9e3a44305b337ab09aaf492
anuradha <>
2006-01-31 06:45:51 +0000

    Name change.

--- scim/trans/cleanup.sh
+++ scim/trans/cleanup.sh
@@ -1,8 +1,8 @@
 [ -f Makefile ] && make distclean
 rm -rf aclocal.m4 config.guess config.h.in config.sub configure \
 	config.rpath install-sh depcomp missing ltmain.sh mkinstalldirs \
-	*~ autom*.cache/ debian/scim-sinhala/ build-stamp \
-	debian/scim-sinhala.substvars debian/files config.status
+	*~ autom*.cache/ debian/scim-sinhala-trans/ build-stamp \
+	debian/scim-sinhala-trans.substvars debian/files config.status
 find -name 'Makefile.in' -exec rm {} \;
 find -name 'Makefile' -exec rm {} \;
 find -name '.libs' -prune -exec rm -rf {} \;

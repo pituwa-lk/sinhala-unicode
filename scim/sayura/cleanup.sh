013be86bf34b03b7dfdd3c7008b45d319020f826
aratnaweera <>
2008-05-28 02:55:30 +0000

    Distclean doesn't clean doc.

--- scim/sayura/cleanup.sh
+++ scim/sayura/cleanup.sh
@@ -1,3 +1,4 @@
+[ -f Makefile ] && make clean
 [ -f Makefile ] && make distclean
 rm -rf aclocal.m4 config.guess config.h.in config.sub configure \
 	config.rpath install-sh depcomp missing ltmain.sh mkinstalldirs \
@@ -8,3 +9,4 @@ find -name 'Makefile.in' -exec rm {} \;
 find -name 'Makefile' -exec rm {} \;
 find -name '.libs' -prune -exec rm -rf {} \;
 find -name '.deps' -prune -exec rm -rf {} \;
+
